6ce8ac90ff7eb2e994a2c45deb1eae56c551266c
KoketsoMalatji <koketsosean209@gmail.com>
2023-06-21 10:59:37 +0000

    ES6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,21 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-all-reject';
+import uploadPhoto from './5-all-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const res = [];
-    for (const item of values) {
-      res.push({ status: item.status, value: item.value || item.reason });
-    }
-    return res;
-  });
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const arr = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    arr.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    arr.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return arr;
 }
