d373ce0cb6dd85797a28164d29970dfc56d50ee9
KoketsoMalatji <koketsosean209@gmail.com>
2023-06-21 11:36:32 +0000

    ES6

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser() {
+const asyncUploadUser = async () => {
   try {
-    const resFromUploadPhoto = await uploadPhoto();
-    const resFromCreateUser = await createUser();
-    return {
-      photo: resFromUploadPhoto,
-      user: resFromCreateUser,
-    };
-  } catch (err) {
-    return {
-      photo: null,
-      user: null,
-    };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
   }
-}
+};
+
+export default asyncUploadUser;
