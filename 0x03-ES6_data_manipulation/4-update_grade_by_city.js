73a71f1d337048161ead7104581fa0dbbc18c7d7
KoketsoMalatji <koketsosean209@gmail.com>
2023-06-23 13:50:28 +0000

    ES6

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+xport default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((obj) => obj.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((obj) => obj.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...person,
+        grade,
+      };
+    });
+}
